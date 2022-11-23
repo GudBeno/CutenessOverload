ca4713340e6ec374db4ff2e9aea9bcf47bd6cb9e
LukeWardlaw <lukewardlaw@gmail.com>
2022-11-23 05:07:30 +0200

    portal integration

--- Cuteness Overload/Assets/Scripts/gamecontroller.cs
+++ Cuteness Overload/Assets/Scripts/gamecontroller.cs
@@ -2,31 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class gamecontroller : MonoBehaviour
 {
     public int killcount;
-    public Text kills;
-    public PlayerShoot shooter;
-    public PlayerMovement mover;
+    public int killgoal;
+    public Text kills, killgoaltext;
     bool contab = false;
+    public GameObject goop, openportaltext, killcard;
     // Start is called before the first frame update
     void Start()
     {
+        goop.SetActive(false);
+        killgoaltext.text = "/" + killgoal.ToString();
+        openportaltext.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         kills.text = killcount.ToString();
-        if (killcount >= 30)
+        if (killcount >= killgoal)
         {
             youwin();
         }
     }
     public void youwin()
     {
-        SceneManager.LoadScene("win scene"); 
+        killcard.SetActive(false);
+        openportaltext.SetActive(true);
+        goop.SetActive(true);
     }
 }
